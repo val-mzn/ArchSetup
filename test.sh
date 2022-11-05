140cbab5a0676f9c46a65982742b00b219d9be43
val_mzn <valentinomanzon@gmail.com>
2022-11-05 17:20:25 +0100

    et

---
+++ test.sh
@@ -0,0 +1,18 @@
+reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+
+parted --script /dev/sda -- mklabel gpt \
+  mkpart ESP fat32 1Mib 513Mib \
+  set 1 boot on \
+  mkpart primary linux-swap 513Mib 4609Mib \
+  mkpart primary ext4 4609Mib 100%
+
+mkfs.vfat -F32 /dev/sda1
+mkswap /dev/sda2
+swapon /dev/sda2
+mkfs.ext4 /dev/sda3
+
+mount /dev/sda3 /mnt
+pacstrap /mnt base base-devel openssh linux linux-firmware neovim
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
