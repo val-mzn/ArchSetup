1a80bb92f3d0da0636b0a99abaa0e10aa159e7db
val_mzn <valentinomanzon@gmail.com>
2022-11-05 17:12:29 +0100

    test

--- setup_script.sh
+++ setup_script.sh
@@ -35,47 +35,13 @@ passwd val_mzn
 sed -i '/NOPASSWD/!s/# %wheel/%wheel/g' /etc/sudoers
 
 pacman -S dhcpcd networkmanager network-manager-applet
-systemctl enable sshd dhcpcd NetworkManager fstrim.timer
+systemctl enable sshd dhcpcd NetworkManager
 
-# BOOT GRUB STUFF
 mount /dev/sda1 /boot
 pacman -S grub-efi-x86_64 efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch
 grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -S iw wpa_supplicant dialog intel-ucode git reflector lshw unzip htop
-pacman -S wget pulseaudio alsa-utils alsa-plugins pavucontrol xdg-user-dirs
-
-su - val_mzn
-xdg-user-dirs-update
-mkdir Sources
-cd Sources
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-
-yay -S pa-applet-git
-
-sudo pacman -S xorg-server xorg-apps xorg-xinit
-sudo pacman -S i3-gaps i3blocks i3lock numlockx
-sudo pacman -S lightdm lightdm-gtk-greeter --needed
-sudo systemctl enable lightdm
-
-sudo pacman -S noto-fonts ttf-ubuntu-font-family ttf-dejavu ttf-freefont
-sudo pacman -S ttf-liberation ttf-droid ttf-roboto terminus-font
-sudo pacman -S rxvt-unicode ranger rofi dmenu --needed
-
-sudo pacman -S firefox --needed
-sudo pacman -S zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-sudo pacman -S lxappearance, arc-gtk-theme, papirus-icon-theme
-
-echo [greeter] >> /etc/lightdm/lightdm-gtk-greeter.conf
-echo theme-name = Arc-Dark >> /etc/lightdm/lightdm-gtk-greeter.conf
-echo icon-theme-name = Papirus-Dark >> /etc/lightdm/lightdm-gtk-greeter.conf
-echo 'background = #2f343f' >> /etc/lightdm/lightdm-gtk-greeter.conf
-
-exit
 exit
 umount -R /mnt
 swapoff /dev/sda2
