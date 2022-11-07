60cdbd98d3ac3721f708064e01e091c11c669433
val_mzn <valentinomanzon@gmail.com>
2022-11-07 16:12:34 +0100

    LAST

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
+pacmam -Sy
+pacman -S dialog
+
 hostname=$(dialog --stdout --inputbox "Enter hostname" 0 0) || exit 1
-clear
-: ${hostname:?"hostname cannot be empty"}
+clear : ${hostname:?"hostname cannot be empty"}
 
 user=$(dialog --stdout --inputbox "Enter admin username" 0 0) || exit 1
-clear
-: ${user:?"user cannot be empty"}
+clear : ${user:?"user cannot be empty"}
 
 password=$(dialog --stdout --passwordbox "Enter admin password" 0 0) || exit 1
-clear
-: ${password:?"password cannot be empty"}
+clear : ${password:?"password cannot be empty"}
 password2=$(dialog --stdout --passwordbox "Enter admin password again" 0 0) || exit 1
 clear
 [[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
@@ -34,10 +34,9 @@ wipefs "${part_boot}"
 wipefs "${part_swap}"
 wipefs "${part_root}"
 
-mkfs.vfat -F32 "${part_boot}"
+mkfs.fat -F32 "${part_boot}"
 mkswap "${part_swap}"
-mkfs.ext4 -f "${part_root}"
-read -p "WAIT"
+mkfs.ext4 "${part_root}"
 
 swapon "${part_swap}"
 mount "${part_root}" /mnt
@@ -46,8 +45,6 @@ mount "${part_boot}" /mnt/boot
 
 pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
-read -p "WAIT"
-clear
 
 {
 	ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
@@ -66,8 +63,8 @@ clear
 	echo ${password} | passwd
 	useradd -m -g users -G wheel,storage,power,audio val_mzn
 	echo ${password} | passwd val_mzn
-	xdg-user-dirs-update
 	sed -i '/NOPASSWD/!s/# %wheel/%wheel/g' /etc/sudoers
+	read -p "END ECHO"
 
 	pacman -S dhcpcd networkmanager network-manager-applet
 	systemctl enable sshd dhcpcd NetworkManager fstrim.timer
@@ -75,8 +72,10 @@ clear
 	pacman -S grub-efi-x86_64 efibootmgr
 	grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch
 	grub-mkconfig -o /boot/grub/grub.cfg
+	read -p "END GRUB"
 
 } | arch-chroot /mnt 
+read -p "WAIT"
 umount -R /mnt
 swapoff /dev/sda2
-reboot
+shutdown
